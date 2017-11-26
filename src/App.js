50218fcccbd2ecd377aabae61a983dca7dacc42c
leifdroms <ldroms@gmail.com>
2017-11-26 01:04:24 -0500

    interim commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
-import dreidel from './dreidel.svg';
 import Scoreboard from './components/scoreboard/scoreboard';
+import Dreidel from './components/dreidel/dreidel';
 import './App.css';
 
 class App extends Component {
   render() {
+    let hebrewLetter = "נ";
     return (
-      <div className="App">
-          <Scoreboard/>
-          <h1 className="App-title">Let's Play Dreidel!</h1>
-          <img src={dreidel} className="dreidel" alt="dreidel"/>
-      </div>
+    <div className="centered">
+    <div>
+    <h1 className="App-title">Let's Play Dreidel!</h1>
+    <Scoreboard/>
+    </div>
+      <Dreidel hebrewLetter={hebrewLetter}/>
+    </div>
     );
   }
 }
---
+++ src/components/dreidel/dreidel.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import '../../App.css';
+import './dreidel.css';
+import dreidel from './dreidel.svg';
+
+class Dreidel extends Component {
+  render() {
+    return (
+    <div>
+     <div className="dreidelContainer">
+          <img src={dreidel} className="dreidel" alt="dreidel"/>
+          <h1 className="hebrew">{this.props.hebrewLetter}</h1>
+      </div>
+      <button className="spinButton" onClick={()=>{alert("hello!")}}>Spin</button>
+      </div>
+      
+    );
+  }
+}
+
+export default Dreidel;
